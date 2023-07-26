bc1c75c8d8ce0e597df51df00ea3f5bb4d27e018
mikbaor <mikbaor@gmail.com>
2023-07-26 13:18:07 -0600

    add arreglado tl

--- Api/src/services/nodemailer/ticketSale.js
+++ Api/src/services/nodemailer/ticketSale.js
@@ -23,6 +23,9 @@ function emailTicketSale({ detailUser, filePath, namePath, res, email }) {
                 user: user,
                 pass: password,
             },
+            tls: {
+                rejectUnauthorized: false,
+              }            
         });
 
 
